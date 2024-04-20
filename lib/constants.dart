30dc429056c7aeedb5038c147f8370b78a93b4d6
safadtm <safadtm2004@gmail.com>
2024-04-20 10:16:58 +0530

    constants --added

---
+++ lib/constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const kactiveCardColor = Color(0xFF1D1E33);
+const kinactiveCardColor = Color(0xFF111328);
+const ksliderInactiveColor = Color(0xFF8D8E98);
+const kbottomContainerHeight = 80.0;
+const kbottomContainerColor = Color(0xFFEB1555);
+const klabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+const kDigitTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
+const klargeBottomButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  color: Colors.white,
+    fontWeight: FontWeight.bold,
+);
+const ktitleTextStyle = TextStyle(
+  fontSize: 50.0,
+    fontWeight: FontWeight.bold,
+);
+const kResultTextStyle = TextStyle(
+  fontSize: 27.0,
+  color: Color(0xFF24D876),
+      fontWeight: FontWeight.bold,
+);
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+const kBodyTextStyle = TextStyle(
+  fontSize: 22,
+);
